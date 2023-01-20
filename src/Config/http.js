00806f0f6564e846fc14c69ab31001b32e676f9b
Mekuzd <emekaseun.es@gmail.com>
2023-01-20 06:21:37 +0100

    installed axios and fetched from mock Api

--- src/Config/http.js
+++ src/Config/http.js
@@ -3,4 +3,5 @@ import axios from "axios";
 const http = axios.create({
   baseURL: "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1",
 });
+
 export default http;
--- src/components/FetchUsers.jsx
+++ src/components/FetchUsers.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
-
-const FetchUsers = () => {
-  let newDate = new Date("2072-12-27T03:44:22.522Z");
-  const [time, settime] = useState(newDate);
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import http from "../Config/http";
+import { BsTextCenter } from "react-icons/bs";
+function FetchUsers() {
+  const [Users, setUsers] = useState([]);
+  const [Loading, setLoading] = useState(true);
+  const [date, setDate] = useState();
 
   let Month = [
     "Jan",
@@ -18,15 +22,86 @@ const FetchUsers = () => {
     "Nov",
     "Dec",
   ];
+  let isMounted = true;
 
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await http("/users");
+        setUsers(response.data);
+        setLoading(false);
+      } catch (error) {
+        setUsers([]);
+      }
+    };
+    if (isMounted) {
+      fetchUsers();
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  console.log(Users);
   return (
-    <table>
-      <p>
-        {Month[time.getMonth()]} {time.getDate()}, {time.getFullYear()}{" "}
-        {time.getHours()}:{time.getSeconds()}{" "}
-        {time.getHours() > 12 ? "AM" : "PM"}
-      </p>
-    </table>
+    <Table responsive>
+      <thead>
+        <tr>
+          <th>
+            <div>
+              {" "}
+              organization <BsTextCenter />
+            </div>
+          </th>
+          <th>
+            <div>
+              Username <BsTextCenter />
+            </div>
+          </th>
+          <th>
+            <div>
+              Email <BsTextCenter />
+            </div>
+          </th>
+          <th>
+            <div>
+              Phone number <BsTextCenter />
+            </div>
+          </th>
+          <th>
+            <div>
+              date joined <BsTextCenter />
+            </div>
+          </th>
+          <th>
+            <div>
+              status <BsTextCenter />
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {Users.map((user) => (
+          <tr key={user.id}>
+            <td>{user?.orgName}</td>
+            <td>{user?.userName}</td>
+            <td>{user?.email}</td>
+            <td>{String(user?.phoneNumber)}</td>
+            <td>
+              {Month[new Date(user.createdAt).getMonth()]}{" "}
+              {new Date(user.createdAt).getDate()},{" "}
+              {new Date(user.createdAt).getFullYear()}{" "}
+              {new Date(user.createdAt).getHours()}:
+              {new Date(user.createdAt).getMinutes() < 10
+                ? `0${new Date(user.createdAt).getMinutes()}`
+                : new Date(user.createdAt).getMinutes()}{" "}
+              {new Date(user.createdAt).getHours() > 12 ? "PM" : "AM"}
+            </td>
+            <td>@mdo</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
-};
+}
+
 export default FetchUsers;
